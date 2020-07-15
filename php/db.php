4886d4bffcc5f5acd1915f689a57f2dd9f81f952
saurabhbehra <39943118+saurabhbehra@users.noreply.github.com>
2020-07-15 20:53:43 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -9,6 +9,7 @@ $con = mysqli_connect($servername, $username, $password,$db);
 
 // Check connection
 if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+   // die("Connection failed: " . mysqli_connect_error());
+    echo "connection failed";
     
 }
